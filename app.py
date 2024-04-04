a09fb715ec6cfa56f02027e006969220849e2bd6
kramaranya <kramaranya15@gmail.com>
2024-04-04 15:07:14 +0100

    Add get method

--- app.py
+++ app.py
@@ -38,5 +38,10 @@ def preprocess():
     return jsonify(preprocessed_data_list)
 
 
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return "Hello! This is a message from your Flask app."
+
+
 if __name__ == '__main__':
     app.run(debug=True)
